06c18444a2094c65030291ea03ac18984b3239bd
jnpn <jnpn@users.noreply.github.com>
2023-01-12 23:58:06 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jnpn"
+original_name="threads"
+original_urlname="threads"
+original_description="Awesome threads created by jnpn"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv threads $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- threads/__main__.py
+++ threads/__main__.py
@@ -1,4 +1,4 @@
-"""Entry point for project_name."""
+"""Entry point for threads."""
 
 from .cli import main  # pragma: no cover
 
